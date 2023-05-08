071bee3f8a926d04eb6672196532cf94a3dd5144
kontb2a22 <ahmedxd.2009@gmail.com>
2023-05-09 02:23:56 +0300

    updating assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./index.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+import styles from "./style";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full overflow-hidden bg-primary">
+      <div className="flex items-center justify-center px-6 sm:px-16">
+        <div className="w-full xl:max-w-[1280px]">Navbar</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="flex items-center justify-center bg-primary">
+        <div className="w-full xl:max-w-[1280px]">hero </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="flex items-center justify-center bg-primary">
+        <div className="w-full xl:max-w-[1280px]">
+          Stats buessiness billing carddeal testimonials clients cta footer
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
